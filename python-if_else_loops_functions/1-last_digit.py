3899d305a73369ae2bca03133d5dd9d30a25e08d
Imarino4 <ignacio.martacos@gmail.com>
2022-09-06 18:51:04 -0700

    negative number fixed

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if number < 0:
-    last_digit = last_digit * -1
-if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-if last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
-if last_digit < 6 and number % 10 != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+n = number
+ld = number % 10
+if n < 0:
+    ld = ld * -1
+if ld > 5:
+    print("Last digit of {} is {} and is greater than 5".format(n, ld))
+if ld == 0:
+    print("Last digit of {} is {} and is 0".format(n, ld))
+if ld < 6 and n % 10 != 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(n, ld))
