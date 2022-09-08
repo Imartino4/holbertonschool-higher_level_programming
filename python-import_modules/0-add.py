4ac0ea96278d793ee47de4757990a8044b4c81b9
Imarino4 <ignacio.martacos@gmail.com>
2022-09-08 15:15:34 -0700

    Task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    #This guard wont run the program when imported
+    # This guard wont run the program when imported
     from add_0 import add
     a = 1
     b = 2
