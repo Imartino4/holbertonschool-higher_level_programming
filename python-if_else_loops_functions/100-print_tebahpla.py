7a7e1c9248eecd8f1eb9062902c837667c788a18
Imarino4 <ignacio.martacos@gmail.com>
2022-09-09 12:01:41 -0700

    Task 14 print alphabet inverse

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# This program print alphabet in reverse order alternating lowercase
+# and upper case, with one prin, one loop and without import any module
+for i in range(90, 64, -1):
+    if i % 2 == 0:
+        c = chr(i + 32)
+    else:
+        c = chr(i)
+    print(f"{c}", end="")
