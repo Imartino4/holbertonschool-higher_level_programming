e276fd69f3238143d880878ea8dfa2668fb040f7
Imarino4 <ignacio.martacos@gmail.com>
2022-09-09 05:06:41 -0700

    1 argument when is only one

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print(f"0 arguments.")
     elif len(sys.argv) > 1:
-        print(f"{len(sys.argv) - 1} arguments:")
+        if len(sys.argv) == 2:
+            print(f"1 argument:")
+        else:
+            print(f"{len(sys.argv) - 1} arguments:")
         for i in range(1, len(sys.argv)):
             print(f"{i}: {sys.argv[i]}")
