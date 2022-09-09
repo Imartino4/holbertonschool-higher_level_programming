59ae9fb8fd53ffe9b9a3b46cfe11cd61db83ac66
Imarino4 <ignacio.martacos@gmail.com>
2022-09-09 11:58:52 -0700

    task 14 advanced

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 def remove_char_at(str, n):
     # creates a copy of the string, removing the character at the position n
-    print(f"{str[0:n]}{str[n + 1:]}")
+    if n >= 0:
+        return str[0:n] + str[n + 1:]
+    else:
+        return str
