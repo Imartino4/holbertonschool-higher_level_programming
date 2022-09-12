4f353579d3dcd753ab4fe61e3248978bbbd62f2e
Imarino4 <ignacio.martacos@gmail.com>
2022-09-12 07:25:59 -0700

    Task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    m = len(matrix)
+    n = len(matrix[0])
+    for i in range(m):
+        for j in range(n):
+            if (j < n - 1):
+                print("{}".format(matrix[i][j]), end=" ")
+            else:
+                print("{}".format(matrix[i][j]), end="")
+
+        print("")
