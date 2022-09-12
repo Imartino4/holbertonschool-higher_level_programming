ae54f01aded97bbff47d7574a6852770894a9e74
Imarino4 <ignacio.martacos@gmail.com>
2022-09-12 08:49:15 -0700

    Task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_ = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_:
+            max_ = my_list[i]
+    return max_
