125bbc3ef8b62e21215bb5193f68731c9c6e5778
Imarino4 <ignacio.martacos@gmail.com>
2022-09-14 11:02:10 -0700

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     r_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if isinstance(roman_string, str) == False:
+    if isinstance(roman_string, str) is False:
         return 0
     for i in roman_string:
         if i not in r_dic:
