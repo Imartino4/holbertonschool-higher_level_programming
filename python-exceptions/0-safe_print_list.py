14842251d14b5b733cf5b92a8c1d75dfd1741069
Imarino4 <ignacio.martacos@gmail.com>
2022-09-15 16:34:02 -0700

    Task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            if count < x:
+                print(f"{i}", end="")
+                count = count + 1
+        print(f"")
+    except TypeError as message:
+        print (message)
+    return count
