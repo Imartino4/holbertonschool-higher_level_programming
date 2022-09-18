3523cde2fd1be8a7d714601ae86e08047b7f717e
Imarino4 <ignacio.martacos@gmail.com>
2022-09-18 08:13:28 -0700

    Task 7 advanced, correct output

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as mess:
-        print(mess)
+        print(mess, file = sys.stderr)
         return False
 
