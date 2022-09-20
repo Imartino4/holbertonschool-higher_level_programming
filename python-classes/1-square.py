58a6532103204a8372b2d38d12d166e5169e1f81
Imarino4 <ignacio.martacos@gmail.com>
2022-09-20 09:49:59 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""No modules"""
+
+
+class Square:
+    """Size class
+
+    """
+    def __init__(self, size):
+        """
+        param1 (size): Square's size
+
+        """
+        self.__size = size
