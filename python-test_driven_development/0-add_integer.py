a3b7e97b4da30d85ea4247b8a9bbaddfa3558b36
Imarino4 <ignacio.martacos@gmail.com>
2022-09-21 08:07:26 -0700

    Task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError(a must be an integer)
+    a = int(a)
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError(b must be an integer)
+    b = int(b)
+    return a + b
