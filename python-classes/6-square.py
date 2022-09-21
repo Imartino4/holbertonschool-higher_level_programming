eba7fbe38cf273b7fff44348f85b1ae15c2570e3
Imarino4 <ignacio.martacos@gmail.com>
2022-09-21 06:19:51 -0700

    Task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" No module imported """
+
+
+class Square:
+    """Defining the Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """ Create instance of Square class
+        Args:
+        param1 (size): Square's size, must be an int >= 0
+        param2 (position): muste be a tuple with 2 positive integers
+
+        """
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Public method to return area of square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Property, retrieve size"""
+        return self.__size
+
+    def position(self):
+        """Property, retrieve position"""
+        return self.position
+
+    @size.setter
+    def size(self, value):
+        """Property setter, set a new size"""
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def position(self, value):
+        """Property setter, set position"""
+        if type(value) != tuple or len(value) != 1:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] <= 0 or value[1] <= 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.position= value
+
+    def my_print(self):
+        """ Public instance method that print a square with "#" """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print(f"{'#'}", end="")
+                print()
