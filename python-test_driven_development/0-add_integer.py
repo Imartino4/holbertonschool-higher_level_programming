efbb862e0447c877c3694738e7bc1366f0eedae4
Imarino4 <ignacio.martacos@gmail.com>
2022-09-21 10:18:28 -0700

    Task 0 - change docstyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,7 +14,7 @@ def add_integer(a, b=98):
     Returns:
         int: a+b
     """
-    if a is None or (not isinstance(a, int) and not isinstance(a, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     a = int(a)
     if not isinstance(b, int) and not isinstance(b, float):
