79f4af73b7c827de1ec79433a28fc878601abfce
Imarino4 <ignacio.martacos@gmail.com>
2022-09-21 08:11:45 -0700

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+""" Function to add integers"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, int) or not isinstance(a, float):
+    """
+    add a + b
+    arguments a,b must be int or float
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError(a must be an integer)
     a = int(a)
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError(b must be an integer)
     b = int(b)
     return a + b
