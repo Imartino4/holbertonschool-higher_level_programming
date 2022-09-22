3b1890ae5fb01b8ec717e6e58e46feac8f6a1e51
Imarino4 <ignacio.martacos@gmail.com>
2022-09-22 06:51:14 -0700

    Task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,8 +9,8 @@ def matrix_divided(matrix, div):
         div: int or float, except 0
     Return:
         a new matrix
-    """
-    
+    """ 
+    Err1 = "matrix must be a matrix (list of lists) of integers/floats"
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
@@ -22,7 +22,7 @@ def matrix_divided(matrix, div):
         new_matrix.append([])
         for j in range(len(matrix[0])):
             if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(Err1)
             a = round(matrix[i][j] / div, 2)
             new_matrix[i].append(a)
     return new_matrix
