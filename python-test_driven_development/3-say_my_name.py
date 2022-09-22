891276f5399e3467e764c380eb64a6fb686c4b9c
Imarino4 <ignacio.martacos@gmail.com>
2022-09-22 07:49:22 -0700

    Task 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 
+This module include say_my_name function
+which prints name and last_name given as argument
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+    first_name: string
+    last_name: string
+
+    Print: My name is <first name> <last name>
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    return print(f"My name is {first_name} {last_name}")
