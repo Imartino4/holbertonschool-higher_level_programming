fa3b93fcc94ed5167f0325b83652f7012b4ffe20
Imarino4 <ignacio.martacos@gmail.com>
2022-09-22 13:17:46 -0700

    Task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module include text_indentation function,
+    which modify a string """
+
+
+def text_indentation(text):
+    """This function receives a string and print a new line after a '.' '?' or ':'"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i - 1] == '.' or text[i - 1] == '?' or text[i - 1] == ':':
+            continue
+        print(f"{text[i]}", end="")
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print("\n")
