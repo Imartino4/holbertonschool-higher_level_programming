81a256ab694380c62276dafc143cbdd57fc4f24b
Imarino4 <ignacio.martacos@gmail.com>
2022-09-24 08:00:18 -0700

    Definee privates atributes and propertie and setter for w y h

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__height = value
+        self.__width = value
 
     @property
     def height(self):
@@ -44,4 +44,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__width = value
+        self.__height = value
