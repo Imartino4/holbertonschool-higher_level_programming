0fd4212961aabe4d519d73296101c1c2088c5133
Imarino4 <ignacio.martacos@gmail.com>
2022-09-24 19:41:28 -0700

    Task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,6 +6,7 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    """Tests"""
 
     def test_max_integer(self):
         self.assertEqual(max_integer([10, 5, 19, 8]), 19)
@@ -14,7 +15,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1.5, 4.6, 3.3, 8.7]), 8.7)
 
     def test_empty_list(self):
-        self.assertEqual(max_integer([], None))
+        self.assertEqual(max_integer([]), None)
 
     def test_same_numbers(self):
         self.assertEqual(max_integer([1, 1, 1]), 1)
