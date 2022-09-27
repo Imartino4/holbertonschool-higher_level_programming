0f5ea14131eae9e333d3e44752484b0eaa632f9e
Imarino4 <ignacio.martacos@gmail.com>
2022-09-27 08:26:42 -0700

    public instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module create a new class BaseGeometry"""
+
+
+class BaseGeometry:
+    """New class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
