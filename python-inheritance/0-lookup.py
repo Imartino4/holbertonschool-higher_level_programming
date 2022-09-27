cf4652408d9382726a5bf3ec4bfcc627428190cc
Imarino4 <ignacio.martacos@gmail.com>
2022-09-27 05:53:54 -0700

    Use of dir

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,7 +2,8 @@
 """ This module contain lookup function
     lookup return a list of attributes and
     methods of an object
-    """
+"""
+
 
 def lookup(obj):
     """Receives an object and return a list of
