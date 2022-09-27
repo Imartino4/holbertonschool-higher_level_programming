d6384012e3d2d89e8b5893cd8f720f8ec68adf2d
Imarino4 <ignacio.martacos@gmail.com>
2022-09-27 07:04:17 -0700

    Type of object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contain is_same_object function
+    which evaluate if an instance belongs to a specified class
+"""
+
+def is_same_class(obj, a_class):
+    """Receives an object and a class, and returns
+    True if the object belongs to the class, and False if not """
+
+    if type(obj) == a_class:
+        return True
+    return False
