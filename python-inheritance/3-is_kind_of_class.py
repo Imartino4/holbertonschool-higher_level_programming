81f86d20161a943c5d8441e0194445a70a723590
Imarino4 <ignacio.martacos@gmail.com>
2022-09-27 07:13:44 -0700

    Isinstance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" This module contain is_same_object function
+""" This module contain is_kind_of_class function
     which evaluate if the object is an instance of a class or an instance 
     of a class that inherited from
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Receives an object and a class, and returns
     True if the object belongs to the class, and False if not """
 
