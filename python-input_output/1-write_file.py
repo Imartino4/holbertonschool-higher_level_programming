6f78ca0f5eb592998bc4a0929ed6b9c8930c71d9
Imarino4 <ignacio.martacos@gmail.com>
2022-09-28 09:57:03 -0700

    Write in a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+""" This module contain write_file function,
+which write a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Receives filename and a text to write,
+        return number of characters written"""
+    with open(filename, 'w') as f:
+        return f.write(text)
