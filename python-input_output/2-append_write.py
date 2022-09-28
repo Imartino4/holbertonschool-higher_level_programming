94ded9c7ab746eb51e3dd7b797a3dca896a6bdb8
Imarino4 <ignacio.martacos@gmail.com>
2022-09-28 10:08:06 -0700

    append a text in a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contain append_write function,
+    which append a string to the end"""
+
+
+def append_write(filename="", text="")
+    """Receives a text and the filename, and append the text at
+    the end of the file"""
+    with open(filename, 'a') as f:
+        return f.write(text)
