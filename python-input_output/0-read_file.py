de5ca682f919480a16926d4b4135ccb2ebc737ab
Imarino4 <ignacio.martacos@gmail.com>
2022-09-28 08:40:20 -0700

    read and print text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Function on this module:
+    red_file: reads a text file and print it to stdout
+    """
+
+
+def read_file(filename=""):
+    """ receives the filename as a string and print the text to
+        stdout"""
+
+    with open(filename) as f:
+        print(f.read())
+    f.closed
