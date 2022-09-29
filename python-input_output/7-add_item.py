e12413dfb5809ed11f922785fd2c25d93619ce77
Imarino4 <ignacio.martacos@gmail.com>
2022-09-28 19:04:07 -0700

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Script to add all arguments to a Python list """
+
+
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+try:
+    list = load_from_json_file(filename)
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i])
+    save_to_json_file(list, filename)
+except Exception:
+    list = []
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i])
+    save_to_json_file(list, filename)
