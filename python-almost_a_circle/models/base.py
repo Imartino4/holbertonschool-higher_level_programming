eff886e73188de7e25d56888d9d0e241b5462fd5
Imartino4 <ignacio.martacos@gmail.com>
2022-10-05 11:18:08 -0700

    Base 15- return string empty list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,7 @@ class Base():
         string representation of that list """
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
