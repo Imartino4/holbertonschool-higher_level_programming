823785fe5899459df1c89844421162eded82bddb
Imartino4 <ignacio.martacos@gmail.com>
2022-10-05 11:05:04 -0700

    Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor for rectangle"""
         # Private attributes with setter and getter
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         # Use init method of Base for id
         super().__init__(id)
 
