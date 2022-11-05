3f0fc1f0dea887eee4cdd1ccd76942f41ec7e7db
Imartino4 <ignacio.martacos@gmail.com>
2022-11-05 14:51:16 -0300

    Task 6 - autogenerate id

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- creates db hbtn_0d_usa and table states
+--states: id INT unique, auto generated can't be null and is a primary key
+--states: name varchar(256) not null
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS states (
+    id int UNIQUE AUTO_INCREMENT NOT NULL,
+    name varchar(256) NOT NULL,
+    PRIMARY KEY (id)
+);
