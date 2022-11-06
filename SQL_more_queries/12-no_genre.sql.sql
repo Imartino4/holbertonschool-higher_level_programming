30ce831dba7d11a27f2d4665d92f16fbd4a6cc76
Imartino4 <ignacio.martacos@gmail.com>
2022-11-06 14:19:33 -0300

    Task 12 - left with condition

---
+++ SQL_more_queries/12-no_genre.sql.sql
@@ -0,0 +1,4 @@
+-- list all shows contained in hbtn_0d_tvshows without genre linked
+-- display tv_shows-title - tv_show_genres.genre_id sorted by title asc
+-- only one Select
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
