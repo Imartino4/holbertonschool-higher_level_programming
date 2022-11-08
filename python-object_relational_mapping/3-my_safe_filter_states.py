e4ebf847c8e3d90d72801c4d2dd0279a35fd17d3
Imarino4 <ignacio.martacos@gmail.com>
2022-11-08 09:28:33 -0800

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script to list states elements from a database
+username, password, database and state name searched must
+be given as argv argument in that order
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ Connect to server"""
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER by states.id ASC')
+    datos = cursor.fetchall()
+    for d in datos:
+        if str(d[1][:]) == argv[4]:
+            print(d)
