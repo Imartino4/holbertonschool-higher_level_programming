e1376dc146f0cc5817b38bd4867f9c5b841bbf0f
Imarino4 <ignacio.martacos@gmail.com>
2022-11-08 07:40:57 -0800

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,19 @@
 """
 Script to list elements from a database
 """
-import MySQLdb
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-""" Connect to server"""
-db = MySQLdb.connect(host='localhost', user='root', passwd='root', db='hbtn_0e_0_usa')
+    """ Connect to server"""
+    db = MySQLdb.connect(
+        host = 'localhost',
+        user = argv[1],
+        passwd = argv[2],
+        db = argv[3])
 
-def list_states():
-    db.cursor().execute('SELECT * FROM states ORDER by states.id ASC')
-print(db)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER by states.id ASC')
+    datos = cursor.fetchall()
+    for d in datos:
+        print(d)
