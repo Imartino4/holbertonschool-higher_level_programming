b9bed86f0a1cfe257a5edd8f44824701b5f9054c
tato <ignacio.martacos@gmail.com>
2022-11-08 12:13:46 -0300

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,4 +7,6 @@ import MySQLdb
 """ Connect to server"""
 db = MySQLdb.connect(host='localhost', user='root', passwd='root', db='hbtn_0e_0_usa')
 
+def list_states():
+    db.cursor().execute('SELECT * FROM states ORDER by states.id ASC')
 print(db)
