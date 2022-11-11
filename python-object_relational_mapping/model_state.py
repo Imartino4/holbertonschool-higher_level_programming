833b6959cf76623805f54b12e636fb666c09a8b2
tato <ignacio.martacos@gmail.com>
2022-11-11 14:07:45 -0300

    Task 6 - class documented

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,8 +13,9 @@ Base = declarative_base()
 
 class State(Base):
     """State class/table"""
+    
+    
     __tablename__ = 'states'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    
