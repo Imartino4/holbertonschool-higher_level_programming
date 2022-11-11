709dbb08211bd9617948379b666bd014baecaa27
tato <ignacio.martacos@gmail.com>
2022-11-11 13:33:37 -0300

    Task 6, 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+"""
+This script list all State objects from 
+database hbtn_0e_6_usa
+username, password and db name must be given
+as argv arguments
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    
+    """Conecto db con la aplicacion"""
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
+    
+    """Creo las tablas"""
+    Base.metadata.create_all(engine)
+    
+    """Relacionar conexion y modelos"""
+    Session = sessionmaker(engine)
+    session = Session()
+    
+    for instance in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
+    
