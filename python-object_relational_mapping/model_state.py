40977eabbdcc921223ba1c0952d4239c5b16d7e2
tato <ignacio.martacos@gmail.com>
2022-11-11 14:09:46 -0300

    Task 6 - class documented

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,11 +11,12 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
+
+
     """State class/table"""
-    
-    
     __tablename__ = 'states'
-    
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    
