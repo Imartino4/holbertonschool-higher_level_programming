74228f54cfa25f1b93336f84a1686fd5f83bb20a
Imarino4 <ignacio.martacos@gmail.com>
2022-11-12 08:14:49 -0800

    Task 8 pep8 fixed

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 This script print the first State object from db hbtn0e_6_usa
 user | password | database must be given as argv
@@ -6,20 +7,20 @@ Not allowed to fetch all states before display results
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine(
         f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
-    
+
     Base.metadata.create_all(engine)
-    
+
     Session = sessionmaker(engine)
     session = Session()
-    
-    user = session.query(State).filter(states.id == 1).first()
-    
+
+    user = session.query(State).filter(State.id == 1).first()
+
     if user:
-        print(f"{states.id}: {states.name}")
+        print(f"{user.id}: {user.name}")
     else:
-        print(Nothing)
-        
+        print("Nothing")
