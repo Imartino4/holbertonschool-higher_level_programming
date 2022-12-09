e2d27d21e796bcd2f7d6bd4814eb36d48b69170a
Imartino4 <ignacio.martacos@gmail.com>
2022-12-09 00:22:38 +0000

    T 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const process = require('process');
 
-if (process.argv.length > 2) {
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
