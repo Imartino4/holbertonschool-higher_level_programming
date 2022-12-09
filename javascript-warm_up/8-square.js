bf1a38bb65fa2d99de5624dfd6d8104fd31f4d0e
Imartino4 <ignacio.martacos@gmail.com>
2022-12-09 13:50:16 +0000

    Task 8 - correct output and semistandard

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv[2];
+
+// One loop
+if (parseInt(args)) {
+  'X'.repeat(parseInt(args));
+  for (let i = 0; i < args; i++) {
+    console.log(Array(parseInt(args) + 1).join('X'));
+  }
+} else {
+  console.log('Missing size');
+}
+
+// Two loops
+// if (parseInt(args)) {
+//   let row = 'X'
+//   for (let i = 0; i < args - 1; i++) {
+//     row = row + 'X' 
+//   }
+//   for (let i = 0; i < args; i++) {
+//     console.log(row);
+//   }
+// } else {
+//   console.log('Missing size');
+// }
