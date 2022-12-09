ae962ec21e4607776d5179c12874720090d25852
Imartino4 <ignacio.martacos@gmail.com>
2022-12-09 18:53:38 +0000

    Task 11 - fixed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 
 const process = require('process');
-const list = process.argv.sort();
-if (list.length > 3) {
-  console.log(list[process.argv.length - 2]);
+const list = [];
+for (let i = 2; i < process.argv.length; i++) {
+  list.push(parseInt(process.argv[i]));
+}
+if (list.length > 1) {
+  list.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(list[list.length - 2]);
 } else {
   console.log('0');
 }
