fd41c96a03e7c273683792b94195506a36570ece
Imartino4 <ignacio.martacos@gmail.com>
2022-12-09 20:53:58 +0000

    Task 5, 6 - ok

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
