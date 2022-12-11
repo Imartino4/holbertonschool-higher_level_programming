c91869e6d8a048ccc9f06e7fdab0b87befd3d134
Imartino4 <ignacio.martacos@gmail.com>
2022-12-11 21:09:39 +0000

    Advanced tasks - 102

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const process = require('process');
-const fs = require('fs') // Allow work with file system(read, wirte, etc.)
-const fA = process.argv[2].toString(); //File to read and copy
+const fs = require('fs'); // Allow work with file system(read, wirte, etc.)
+const fA = process.argv[2].toString(); // File to read and copy
 const fB = process.argv[3].toString(); // File to read and copy
-const fC = process.argv[4]; //File to create and write
+const fC = process.argv[4]; // File to create and write
 
-//readFileSync ya retorna el contenido
+// readFileSync ya retorna el contenido
 
-fs.writeFileSync(fC, fs.readFileSync(fA) + '\n' + fs.readFileSync(fB));
+fs.writeFileSync(fC, fs.readFileSync(fA) + fs.readFileSync(fB));
