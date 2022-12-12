b7ea8603357cce1a1066e79dcc48778421242653
Imartino4 <ignacio.martacos@gmail.com>
2022-12-12 15:22:49 +0000

    Task 4 - local ok

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const process = require('process');
 const request = require('request');
 const url = process.argv[2];
-const charId = 'https://swapi-api.hbtn.io/api/people/18/';
+const charId = '18';
 
 request(url, (error, res, body) => {
   if (error) {
@@ -13,7 +13,7 @@ request(url, (error, res, body) => {
   let count = 0;
   for (let i = 0; i < results.length; i++) {
     for (let j = 0; j < results[i].characters.length; j++) {
-      if (results[i].characters[j] === charId) {
+      if (results[i].characters[j].includes(charId)) {
         count++;
       }
     }
