cd3142dc4180616231375b0ca7a04f06e0bf55c8
Imartino4 <ignacio.martacos@gmail.com>
2022-12-12 13:39:27 +0000

    Task 2 - output ok-

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response) {
+  if (!error) {
+    console.log('code:', response && response.statusCode);
+  }
+});
