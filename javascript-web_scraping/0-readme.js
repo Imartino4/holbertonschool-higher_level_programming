cf5da9f8e76417ce143bec79747687db015cf6f3
Imartino4 <ignacio.martacos@gmail.com>
2022-12-12 13:03:01 +0000

    Adding readme and Task 0 -ok-

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs');
+
+const fileA = process.argv[2];
+
+fs.readFile( fileA, 'utf8', function(err, data) {
+    if (err) {
+        console.error(err);
+        return;
+      }
+    console.log(data);
+});
