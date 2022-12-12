0a35168441826c33de856bcdeb1ce6ec27d2ec3c
Imartino4 <ignacio.martacos@gmail.com>
2022-12-12 14:22:15 +0000

    Task 3 - output ok

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,6 @@ const process = require('process');
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-console.log(url);
-
 request.get(url, (error, res, body) => {
   if (error) {
     throw error;
